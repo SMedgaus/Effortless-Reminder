f4bc36187220ce7efab18df1fc75872e36abbd6d
Sergey Medgaus <sergey.medgaus@gmail.com>
2020-11-09 21:34:10 +0200

    Add empty Android project

---
+++ app/src/main/java/com/giniapps/effortless_reminder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.giniapps.effortless_reminder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
